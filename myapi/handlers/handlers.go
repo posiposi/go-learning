1a2d5ab1fdf19806bbaaf260f808680250fd734f
daichi.sugiyama <caad12.di2@gmail.com>
2024-05-01 18:39:17 +0900

    初期セットアップ

---
+++ myapi/handlers/handlers.go
@@ -0,0 +1,76 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/yourname/reponame/models"
+)
+
+// Request is GET
+func HelloHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello, world!\n")
+}
+
+func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+	var reqArticle models.Article
+
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+		return
+	}
+
+	article := reqArticle
+	if err := json.NewEncoder(w).Encode(article); err != nil {
+		http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
+		return
+	}
+}
+
+func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+	queryMap := req.URL.Query()
+	var page int
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		var err error
+		page, err = strconv.Atoi(p[0])
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		page = 1
+	}
+	resString := fmt.Sprintf("Article List (page %d)\n", page)
+	io.WriteString(w, resString)
+}
+
+func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+	}
+	resString := fmt.Sprintf("Article No.%d\n", articleID)
+	io.WriteString(w, resString)
+}
+
+func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+	var reqArticle models.Article
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+	}
+	article := reqArticle
+	json.NewEncoder(w).Encode(article)
+}
+
+func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	var reqComment models.Comment
+	if err := json.NewDecoder(req.Body).Decode(&reqComment); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+	}
+	article := reqComment
+	json.NewEncoder(w).Encode(article)
+}
---
+++ myapi/models/models.go
@@ -0,0 +1,55 @@
+package models
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type Comment struct {
+	CommentID int       `json:"comment_id"`
+	ArticleID int       `json:"article_id"`
+	Message   string    `json:"message"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type Article struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Contents    string    `json:"contents"`
+	UserName    string    `json:"user_name"`
+	NiceNum     int       `json:"nice_num"`
+	CommentList []Comment `json:"comment_list"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+func main() {
+	comment1 := Comment{
+		CommentID: 1,
+		ArticleID: 1,
+		Message:   "test comment1",
+		CreatedAt: time.Now(),
+	}
+	comment2 := Comment{
+		CommentID: 2,
+		ArticleID: 1,
+		Message:   "second comment",
+		CreatedAt: time.Now(),
+	}
+	article := Article{
+		ID:          1,
+		Title:       "first article",
+		Contents:    "This is the first article",
+		UserName:    "saki",
+		NiceNum:     1,
+		CommentList: []Comment{comment1, comment2},
+		CreatedAt:   time.Now(),
+	}
+	jsonData, err := json.Marshal(article)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("%s\n", jsonData)
+}
